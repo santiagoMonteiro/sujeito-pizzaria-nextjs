6cdcf33b5d5d4a3e1358122c15975654f176b5b9
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2022-06-01 20:26:12 -0400

    registration page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,35 +1,39 @@
 import Head from "next/head";
 import Image from "next/image";
 
-import styles from "./home.module.scss";
+import styles from "../home.module.scss";
 
-import logoImg from "../../public/logo.svg";
+import logoImg from "../../../public/logo.svg";
 
-import { Input } from "../components/ui/Input";
-import { Button } from "../components/ui/Button";
+import { Input } from "../../components/ui/Input";
+import { Button } from "../../components/ui/Button";
 
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>SujeitoPizza - Faça seu login</title>
+        <title>SujeitoPizza - Faça seu cadastro</title>
       </Head>
 
       <div className={styles.container}>
         <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
 
         <div className={styles.formContainer}>
+
+        <h1>Criando sua conta</h1>
           <form>
+            <Input placeholder="Digite seu nome" type="text" />
             <Input placeholder="Digite seu email" type="email" />
             <Input placeholder="Sua senha" type="password" />
-            <Button type="submit">Acessar</Button>
+
+            <Button type="submit">Cadastrar</Button>
           </form>
 
-          <Link href="/signup">
+          <Link href="/">
             <a className={styles.registrationLink}>
-              Não possui uma conta? Cadastre-se
+              Já possui uma conta? Faça login
             </a>
           </Link>
         </div>
